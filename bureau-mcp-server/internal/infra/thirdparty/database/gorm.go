49abb8d65cc9ac6317c61f4e468752df5dfe4d91
BrunoPolaski <polaskibruno03@gmail.com>
2026-06-14 11:11:49 -0300

    feat: adding whole dsn inside gorm file

--- bureau-mcp-server/internal/infra/thirdparty/database/gorm.go
+++ bureau-mcp-server/internal/infra/thirdparty/database/gorm.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -17,40 +16,23 @@ func NewGormAdapter() *gormAdapter {
 }
 
 func (m *gormAdapter) Connect() *gorm.DB {
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USERNAME")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-
-	fmt.Printf("Connecting to database with host=%s, port=%s, user=%s, dbname=%s\n", dbHost, dbPort, dbUser, dbName)
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
-		dbHost,
-		dbUser,
-		dbPass,
-		dbName,
-		dbPort,
-	)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 
-	// Configure connection pool
 	sqlDB, err := db.DB()
 	if err != nil {
 		log.Fatalf("failed to get database instance: %v", err)
 	}
 
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
 	sqlDB.SetMaxIdleConns(10)
-
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
 	sqlDB.SetMaxOpenConns(100)
-
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	return db
